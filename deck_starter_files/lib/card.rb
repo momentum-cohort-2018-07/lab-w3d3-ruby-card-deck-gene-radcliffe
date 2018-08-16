c969d027c8b8b5c7ce5cb61cdb827a28f298a00d
gene-radcliffe <g_radcliffe@hotmail.com>
2018-08-16 09:06:03 -0400

    Recommitting: I created the wrong repo

--- deck_starter_files/lib/card.rb
+++ deck_starter_files/lib/card.rb
@@ -1,2 +1,55 @@
+require 'set'
+
 class Card
+    include Comparable
+    attr_accessor :rank, :suit
+    
+    def initialize(rank, suit)
+        @rank = rank
+        @suit = suit
+        @card = nil
+        
+        @rank_hash={
+            :A=> 1, 
+            2=> 2,
+            3=> 3,
+            4=> 4,
+            5=> 5,
+            6=> 6,
+            7=> 7,
+            8=> 8,
+            9=> 9,
+            10=> 10,
+            :J=> 11,
+            :Q=>12,
+            :K=> 13,
+            }
+    
+    end
+
+    def <=>(other)
+        
+         return other.rank == self.rank && other.suit == self.suit 
+  
+    end
+
+    def greater_than?(other_card)
+        other_card_rank = get_rank_from_hash(other_card.rank)
+        this_card_rank = get_rank_from_hash(@rank)
+  
+        if this_card_rank > other_card_rank then
+          true
+        else
+          false
+        end
+    end
+  
+      private
+      def get_rank_from_hash(card_rank)
+         #@rank_hash[card_rank.to_s.to_sym]
+         @rank_hash[card_rank]
+      end
+  
+    
+    
 end
--- deck_starter_files/lib/deck.rb
+++ deck_starter_files/lib/deck.rb
@@ -1,4 +1,79 @@
 require_relative "card"
-
 class Deck
-end
+    include Comparable
+    attr_accessor :cards_left, :shuffled_deck
+   
+
+ 
+
+    def init_hash
+      @DECK_HASH = {
+      
+      clubs: [:A,2,3,4,5,6,7,8,9,10,:J,:Q,:K],
+      diamonds: [:A,2,3,4,5,6,7,8,9,10,:J,:Q,:K],
+      hearts: [:A,2,3,4,5,6,7,8,9,10,:J,:Q,:K],
+      spades: [:A,2,3,4,5,6,7,8,9,10,:J,:Q,:K]
+      }
+    
+    end
+    
+    def init_deck
+        @card_array=[]
+        create_deck(@card_array)
+        @cards_left = @card_array.length
+        @card_counter =0
+        @current_card=nil
+        @shuffled_deck =[]
+    end
+    
+    def initialize
+    
+      init_hash
+      init_deck
+      
+    end
+
+    def draw
+        @current_card = get_card(@card_counter)
+        delete_card(@card_counter)
+        @card_counter += 1
+        @current_card
+    end
+    def cards
+        @card_array
+    end
+
+    def <=>(other)
+        
+    end
+    def shuffle
+        @shuffled_deck = @card_array.shuffle
+        @shuffled_deck
+    end
+
+    def delete_card(index)
+        @card_array.delete_at(index)
+        @cards_left = @card_array.length
+    end
+
+    def find_card(card)
+      index_of_card = @card_array.find_index(card)
+      index_of_card
+    end
+
+    
+    def get_card(index)
+        @card_array[index]
+    end
+
+    private
+    def create_deck(cards)
+      card_array=cards
+      @DECK_HASH.each do |k,v|
+        v.each do |val|
+          card_array.push(Card.new(val,k))
+        end
+      end
+    end
+
+end 
